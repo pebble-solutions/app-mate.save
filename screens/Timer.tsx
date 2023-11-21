10b2cdc1bf854b61989a9ab39343e1aec3c112aa
deathtroll22 <deathtroll@laposte.net>
2023-11-21 04:26:10 +0100

    ajouet des cercles conditionnels et misen en page

--- screens/Timer.tsx
+++ screens/Timer.tsx
@@ -6,7 +6,6 @@ import SvgMountains from './SVG/SvgMountains';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Timer: FC = () => {
-  // Modification : stockage des temps avec leurs libellés
   const [pressTimes, setPressTimes] = useState<{ time: Date; label: string }[]>([]);
 
   useEffect(() => {
@@ -26,36 +25,26 @@ const Timer: FC = () => {
 
   const onPressSun = async () => {
     const currentTime = new Date();
-    // Ajouter le nouveau clic sans définir le libellé pour le moment
     setPressTimes([...pressTimes, { time: currentTime, label: "" }]);
   };
-  
+
   useEffect(() => {
-    // Mettre à jour les libellés une fois que pressTimes est mis à jour
     const updatedPressTimes = pressTimes.map((item, index, array) => {
       if (index === 0) {
         return { ...item, label: "Début d'activité" };
       } else if (index % 2 === 1 && index !== array.length - 1) {
         return { ...item, label: "Pause" };
-    } else if (index % 2 === 0) {
+      } else if (index % 2 === 0) {
         return { ...item, label: "Reprise" };
-      }
-      else {
-        return { ...item, label: "Fin d'activité" };
+      } else {
+        return { ...item, label: "Arrêt de l'activité" };
       }
     });
-  
-    // Vérifier si la mise à jour est nécessaire
+
     if (updatedPressTimes.some((item, index) => item.label !== pressTimes[index]?.label)) {
       setPressTimes(updatedPressTimes);
     }
-  }, [pressTimes]); // Dépend de pressTimes
-  
-  
-  
-  
-  
-  
+  }, [pressTimes]);
 
   const clearPressTimes = async () => {
     try {
@@ -70,10 +59,34 @@ const Timer: FC = () => {
     // Votre logique ici
   };
 
-  const renderItem = ({ item }: { item: { time: Date; label: string }; index: number }) => (
-    <Text style={styles.lastPressTimeText}>
-      {item.label} : {item.time.toLocaleTimeString()}
-    </Text>
+  const renderItem = ({ item, index }: { item: { time: Date; label: string }; index: number }) => (
+    <View style={styles.listItem}>
+      <View style={styles.timeline}>
+        {/* Afficher verticalLine seulement si ce n'est ni le premier ni le dernier élément */}
+        {index !== 0 && index !== pressTimes.length - 1 && <View style={styles.verticalLine} />}
+        
+        {/* Logique existante pour afficher le cercle */}
+        {index === 0 ? (
+          <View style={styles.largeCircleBorder}>
+            <View style={styles.largeCircleLine} />
+          </View>
+        ) : item.label === "Arrêt de l'activité" ? (
+          <View style={styles.largeCircleFilled} />
+        ) : (
+          <View style={styles.smallCircleFilled} />
+        )}
+      </View>
+      <View style={styles.labelContainer}>
+      <Text style={[styles.labelText, item.label === "Pause" || item.label === "Reprise" ? styles.opaqueLabel : null]}>
+        {item.label}
+      </Text>
+    </View>
+      <View style={styles.timeContainer}>
+        <Text style={styles.timeText}>{item.time.toLocaleTimeString()}</Text>
+      </View>
+      {/* Afficher lastItemLine seulement si c'est le dernier élément et la longueur de pressTimes est >= 2 */}
+      {index === pressTimes.length - 1 && pressTimes.length >= 2 && <View style={styles.lastItemLine} />}
+    </View>
   );
 
   return (
@@ -90,8 +103,7 @@ const Timer: FC = () => {
       >
         <SvgSun />
       </TouchableOpacity>
-      
-      {/* Utilisation de FlatList pour afficher la liste des clics */}
+
       <FlatList
         data={pressTimes}
         renderItem={renderItem}
@@ -99,7 +111,6 @@ const Timer: FC = () => {
         style={styles.listContainer}
       />
 
-      {/* Bouton "Valider" */}
       <TouchableOpacity
         onPress={onValidatePress}
         style={[styles.button, styles.validateButton]}
@@ -107,7 +118,6 @@ const Timer: FC = () => {
         <Text style={styles.buttonText}>Valider</Text>
       </TouchableOpacity>
 
-      {/* Bouton "Annuler" */}
       <TouchableOpacity
         onPress={clearPressTimes}
         style={[styles.button, styles.clearButton]}
@@ -117,7 +127,6 @@ const Timer: FC = () => {
     </LinearGradient>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -147,10 +156,69 @@ const styles = StyleSheet.create({
     maxHeight: '100%',
     padding: 10,
   },
-  lastPressTimeText: {
+  listItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  timeline: {
+    alignItems: 'center',
+  },
+  verticalLine: {
+    width: 1,
+    height: 25,
+    backgroundColor: 'white',
+    position: 'absolute',
+    zIndex: 1,
+    bottom: 3,
+  },
+  largeCircleBorder: {
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    borderWidth: 3,
+    borderColor: 'white',
+    right: 2
+  },
+  lastItemLine: {
+    width: 1,
+    height: 22,
+    backgroundColor: 'white', // Couleur de la ligne pour le dernier élément
+    position: 'absolute',
+    zIndex: 1,
+    bottom: 12,
+    left: 4,
+  },
+  largeCircleFilled: {
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    backgroundColor: 'white',
+    right: 3,
+  },
+  smallCircleFilled: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: 'white',
+    left: 0,
+  },
+  labelContainer: {
+    marginLeft: 10,
+  },
+  labelText: {
     color: 'white',
     fontSize: 16,
-    textAlign: 'center',
+  },
+  opaqueLabel: {
+    opacity: 0.7,
+  },
+  timeContainer: {
+    marginLeft: 'auto',
+  },
+  timeText: {
+    color: 'white',
+    fontSize: 16,
   },
   button: {
     position: 'relative',
