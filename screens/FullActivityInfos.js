3b3c9c6f047c891edc8e28aef9506f54629bf36e
deathtroll22 <deathtroll@laposte.net>
2023-12-25 19:42:35 +0100

    wip sur le contenu des activity full infos

--- screens/FullActivityInfos.js
+++ screens/FullActivityInfos.js
@@ -1,16 +1,37 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 import moment from 'moment';
 import DeleteActivityButton from '../components/deleteActivityButton';
 
 const FullActivityInfos = ({ activity, onClose, onDelete }) => {
-    const selectedItem = activity;
+  const selectedItem = activity;
+  const openSettingsModal = () => {
+    console.log('Ouverture des réglages');
+  };
+
+  // Fonction pour générer les carrés verts
+  const renderGreenSquares = () => {
+    const squares = [];
+    for (let i = 0; i < 16; i++) {
+      squares.push(
+        <View key={i} style={styles.greenSquare}>
+          {/* Contenu des carrés, par exemple, un numéro */}
+          <Text style={styles.greenSquareText}>{i + 1}</Text>
+        </View>
+      );
+    }
+    return squares;
+  };
+
   return (
     <View style={{ ...styles.container, backgroundColor: selectedItem.color }}>
       {/* Barre supérieure avec bouton de fermeture */}
       <View style={styles.header}>
-      {/* Bouton de suppression */}
-    
+        {/* Bouton "Réglages" pour ouvrir la BottomSheet */}
+        <TouchableOpacity onPress={openSettingsModal} style={styles.settingsButton}>
+          <Text style={styles.settingsButtonText}>Réglages</Text>
+        </TouchableOpacity>
+        {/* Bouton de suppression */}
         <TouchableOpacity onPress={onClose} style={styles.closeButton}>
           <Text style={styles.closeButtonText}>Fermer</Text>
         </TouchableOpacity>
@@ -22,13 +43,37 @@ const FullActivityInfos = ({ activity, onClose, onDelete }) => {
         <Text style={styles.activityDate}>Créé le {moment(activity.start).format('DD.MM.YYYY')}</Text>
         <Text style={styles.activityDate}>{activity.description}</Text>
         {/* Ajoutez ici les autres informations de l'activité */}
-      </View>  
-      
-      <DeleteActivityButton
-            title={selectedItem.label}
-            id={selectedItem._id}
-          />
+      </View>
 
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.infoContainer}>
+          <Text style={styles.infoSectionTitle}>Variables liées</Text>
+          <Text style={styles.infoSectionContent}>- variable 1</Text>
+          <Text style={styles.infoSectionContent}>- variable 2</Text>
+          <Text style={styles.infoSectionContent}>- variable 3</Text>
+        </View>
+        {/* Contenu de la première section */}
+        <View style={styles.infoContainer}>
+          <Text style={styles.infoSectionTitle}>Collaborateurs</Text>
+          <Text style={styles.infoSectionContent}>- colab 1</Text>
+          <Text style={styles.infoSectionContent}>- colab 2</Text>
+          <Text style={styles.infoSectionContent}>- colab 3</Text>
+          {/* Contenu de la deuxième section */}
+        </View>
+        <View style={styles.infoContainer}>
+          <Text style={styles.infoSectionTitle}>Autres</Text>
+          {/* Affichage des carrés verts */}
+          <View style={styles.greenSquaresContainer}>{renderGreenSquares()}</View>
+        </View>
+        {/* Contenu de la troisième section */}
+        <DeleteActivityButton
+          title={selectedItem.label}
+          id={selectedItem._id}
+        />
+      </ScrollView>
+
+      {/* Cette View garantit que le ScrollView termine à 10% du bas de l'écran */}
+      <View style={{ height: '10%' }} />
     </View>
   );
 };
@@ -36,52 +81,73 @@ const FullActivityInfos = ({ activity, onClose, onDelete }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    padding: 16,
   },
   header: {
     flexDirection: 'row',
-    justifyContent: 'flex-end',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 25,
   },
-  closeButton: {
-    padding: 10,
+  settingsButtonText: {
+    color: 'white',
+    fontSize: 14,
   },
   closeButtonText: {
     color: 'white',
-    marginTop: 15,
-    fontSize: 16,
+    fontSize: 14,
   },
   activityContent: {
-    flex: 1,
-   
     alignItems: 'center',
+    marginTop: 20,
   },
   activityName: {
     color: 'white',
     fontWeight: 'bold',
     fontSize: 24,
-    marginBottom: 10,
   },
   activityDate: {
     color: 'white',
     fontSize: 16,
   },
-  deleteButtonContainer: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    marginBottom: 20, // Espacement en bas de la page
-  },
-  deleteButton: {
-    backgroundColor: 'red',
-    borderRadius: 10,
-    paddingVertical: 10,
-    alignItems: 'center',
+  scrollView: {
     marginTop: 20,
   },
-  deleteButtonText: {
+  infoContainer: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    borderRadius: 10,
+    padding: 20,
+    marginBottom: 12,
+  },
+  infoSectionTitle: {
     color: 'white',
-    fontWeight: 'bold',
     fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 15,
+  },
+  infoSectionContent: {
+    color: 'white',
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  greenSquaresContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  greenSquare: {
+    width: '48%',
+    height: 80,
+    backgroundColor: 'green',
+    borderRadius: 10,
+    marginBottom: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  greenSquareText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
