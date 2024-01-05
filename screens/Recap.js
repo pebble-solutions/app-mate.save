0db244468a2d281c7385715ebaaba4d843751b42
deathtroll22 <deathtroll@laposte.net>
2024-01-05 13:56:31 +0100

    suppression des commentaires et des typages tsx

--- screens/Recap.js
+++ screens/Recap.js
@@ -8,29 +8,6 @@ const Recap = () => {
 	const [selectedItem, setSelectedItem] = useState(null);
 	const [modalVisible, setModalVisible] = useState(false);
   const [activityName, setActivityName] = useState('');
-
-	
-	// useEffect(() => {
-	// 	const fetchData = async () => {
-	// 		try {
-	// 			const fetchAndSetData = async (url(String), setter) => {
-	// 				const response = await fetch(url);
-	// 				const data = await response.json();
-	// 				setter(data);
-	// 				console.log(response, 'response');
-	// 		console.log(data, 'data');
-	// 			};
-				
-	// 			await Promise.all([
-	// 				fetchAndSetData('https://api.pebble.solutions/v5/metric/variable/', setMetrics),
-	// 			]);
-	// 		} catch (error) {
-	// 			console.error('Erreur lors de la récupération des données:', error);
-	// 		}
-	// 	};
-		
-	// 	fetchData();
-	// }, []);
   return (
     <View style={styles.container}>
       <View style={styles.header}>
