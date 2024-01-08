045ade8cf4b19f463157c17b622f0819acc2e912
deathtroll22 <deathtroll@laposte.net>
2024-01-08 20:26:13 +0100

    travail sur le recap , day card , day caroussel, et le menu de bouton

--- screens/cards/DayCard.js
+++ screens/cards/DayCard.js
@@ -1,27 +1,49 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const DayCard = ({ date, morningCheckIn, afternoonCheckIn, workedHours }) => {
+const DayCard = ({ date, morningCheckIn, morningBreak, afternoonCheckIn, afternoonBreak, workedHours, overtimeHours, overtimeRate }) => {
+    const overtimePay = overtimeHours * overtimeRate; // Calculer le paiement des heures supplémentaires
+
     return (
-        <View style={styles.card}>
-            <Text style={styles.cardTitle}>{date}</Text>
-            {/* Affichez les autres propriétés pour tester */}
-            <Text style={styles.textWhite}>{morningCheckIn}</Text>
-            <Text style={styles.textWhite}>{afternoonCheckIn}</Text>
-            <Text style={styles.textWhite}>{workedHours}</Text>
+        <View style={styles.cardContainer}>
+            <View style={styles.card}>
+                <Text style={styles.cardTitle}>{date}</Text>
+                <Text style={styles.textWhite}>Check-in matin : {morningCheckIn}</Text>
+                <Text style={styles.textWhite}>Pause matin : {morningBreak}</Text>
+                <Text style={styles.textWhite}>Check-in après-midi : {afternoonCheckIn}</Text>
+                <Text style={styles.textWhite}>Pause après-midi : {afternoonBreak}</Text>
+                <Text style={styles.textWhite}>Heures travaillées : {workedHours}</Text>
+            </View>
+
+            <View style={styles.card}>
+                <Text style={styles.cardTitle}>Informations et variables de paie :</Text>
+                <Text style={styles.textWhite}>Salaire de base : $2000</Text>
+                <Text style={styles.textWhite}>Heures supplémentaires : {overtimeHours} heures</Text>
+                <Text style={styles.textWhite}>Taux horaire des heures supplémentaires : ${overtimeRate}/h</Text>
+                <Text style={styles.textWhite}>Total des heures supplémentaires : ${overtimePay}</Text>
+                {/* Ajoutez d'autres variables de paie fictives ici */}
+            </View>
+
+            <View style={styles.card}>
+                <Text style={styles.cardTitle}>Commentaires</Text>
+                <Text style={styles.textWhite}>• Ceci est un commentaire sur cette journée.</Text>
+                <Text style={styles.textWhite}>• Un autre commentaire fictif.</Text>
+                {/* Ajoutez d'autres commentaires fictifs ici */}
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-
+    cardContainer: {
+        // Style pour le conteneur global des cartes
+    },
     card: {
-        backgroundColor: 'rgba(37,39,66,255)', // Fond bleu foncé transparent à 30%
+        backgroundColor: 'rgba(37,39,66,255)', // Fond bleu foncé
         margin: 10,
         padding: 15,
         borderRadius: 15,
         color: '#ffffff', // Texte blanc
-        // Autres styles pour les cartes
     },
     cardTitle: {
         fontWeight: 'bold',
@@ -33,7 +55,6 @@ const styles = StyleSheet.create({
     textWhite: {
         color: '#ffffff', // Texte blanc
     },
-
-
 });
+
 export default DayCard;
--- screens/carousel/DayCarousel.js
+++ screens/carousel/DayCarousel.js
@@ -1,66 +1,81 @@
-
-import React, { useState } from 'react';
+import React from 'react';
 import Carousel from 'react-native-snap-carousel';
 import DayCard from '../cards/DayCard';
 import { Dimensions } from 'react-native';
 
 const screenWidth = Dimensions.get('window').width;
-const itemWidth = screenWidth * 0.9; // Par exemple, 90% de la largeur de l'écran
+const itemWidth = screenWidth * 1;
+
+
+
+const convertTimeToMinutes = (time) => {
+    const [hours, minutes] = time.split('h').map(Number);
+    return hours * 60 + minutes;
+};
+
+const calculateDuration = (startTime, endTime) => {
+    return endTime - startTime;
+};
+
+const calculateWorkedHours = (morningCheckIn, morningBreak, afternoonBreak, afternoonCheckIn) => {
+    // Convertir les heures en minutes
+    const morningCheckInMinutes = convertTimeToMinutes(morningCheckIn);
+    const [morningBreakStart, morningBreakEnd] = morningBreak.split(' - ').map(convertTimeToMinutes);
+    const [afternoonBreakStart, afternoonBreakEnd] = afternoonBreak.split(' - ').map(convertTimeToMinutes);
+    const afternoonCheckInMinutes = convertTimeToMinutes(afternoonCheckIn);
+
+    // Calculer la durée travaillée le matin et l'après-midi
+    const morningWorkDuration = calculateDuration(morningCheckInMinutes, morningBreakStart);
+    const morningBreakDuration = calculateDuration(morningBreakStart, morningBreakEnd);
+    const afternoonWorkDuration = calculateDuration(afternoonBreakEnd, afternoonCheckInMinutes);
+    const afternoonBreakDuration = calculateDuration(afternoonBreakStart, afternoonBreakEnd);
+
+    // Calculer le total
+    const totalMinutesWorked = morningWorkDuration + afternoonWorkDuration - morningBreakDuration - afternoonBreakDuration;
+
+    // Convertir le total en heures et minutes
+    const hours = Math.floor(totalMinutesWorked / 60);
+    const minutes = totalMinutesWorked % 60;
+
+    return `${hours}h${minutes}`;
+};
 
 const DayCarousel = () => {
-    // const [daysData, setDaysData] = useState([
-        
-    // {
-    //     date: 'Lundi 1er',
-    //     morningCheckIn: '9h00',
-    //     afternoonCheckIn: '13h00',
-    //     workedHours: '4h00',
-    //   },
-    //   {
-    //     date: 'Mardi 2',
-    //     morningCheckIn: '9h00',
-    //     afternoonCheckIn: '13h00',
-    //     workedHours: '4h00',
-    //   },
-    //   {
-    //     date: 'Mercredi 3',
-    //     morningCheckIn: '9h00',
-    //     afternoonCheckIn: '13h00',
-    //     workedHours: '4h00',
-    //   },
-    //   {
-    //     date: 'Jeudi 4',
-    //     morningCheckIn: '9h00',
-    //     afternoonCheckIn: '13h00',
-    //     workedHours: '4h00',
-    //   },
-    //   {
-    //     date: 'Vendredi 5',
-    //     morningCheckIn: '9h00',
-    //     afternoonCheckIn: '13h00',
-    //     workedHours: '4h00',
-    //   },
-    //   {
-    //     date: 'Samedi 6',
-    //     morningCheckIn: '9h00',
-    //     afternoonCheckIn: '13h00',
-    //     workedHours: '4h00',
-    //   },
-    //   {
-    //     date: 'Dimanche 7',
-    //     morningCheckIn: '9h00',
-    //     afternoonCheckIn: '13h00',
-    //     workedHours: '4h00',
-    //   },
-    // ]);
+    // Données fictives avec des pauses
+    const testDaysData = [
+        {
+            date: 'Lundi 9',
+            morningCheckIn: '8h30',
+            morningBreak: '10h30 - 10h45', // Pause du matin
+            afternoonCheckIn: '14h00',
+            afternoonBreak: '16h00 - 16h15', // Pause de l'après-midi
+            workedHours: '5h30',
+        },
+        {
+            date: 'Mardi 10',
+            morningCheckIn: '9h00',
+            morningBreak: '11h00 - 11h15',
+            afternoonCheckIn: '13h00',
+            afternoonBreak: '15h00 - 15h15',
+            workedHours: '4h00',
+        },
+        {
+            date: 'Mercredi 11',
+            morningCheckIn: '9h15',
+            morningBreak: '11h15 - 11h30',
+            afternoonCheckIn: '13h30',
+            afternoonBreak: '15h30 - 15h45',
+            workedHours: '4h15',
+        },
+        // Vous pouvez ajouter d'autres enregistrements ici...
+    ];
 
-    // Pour le test, utilisons un tableau avec un seul élément
-    const testDaysData = [{
-        date: 'DD de la session',
-        morningCheckIn: 'Test Morning',
-        afternoonCheckIn: 'Test Afternoon',
-        workedHours: 'Test Hours',
-    }];
+       // Calculer les heures travaillées pour chaque jour
+       testDaysData.forEach(day => {
+        day.workedHours = calculateWorkedHours(
+            day.morningCheckIn, day.morningBreak, day.afternoonBreak, day.afternoonCheckIn
+        );
+    });
 
     const renderItem = ({ item }) => <DayCard {...item} />;
 
