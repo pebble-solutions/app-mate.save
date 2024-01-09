2097093d1fd19f938206c09df4d7066390cce989
deathtroll22 <deathtroll@laposte.net>
2024-01-09 21:28:57 +0100

    wip monthcarousel

--- screens/carousel/MonthCarousel.js
+++ screens/carousel/MonthCarousel.js
@@ -1,92 +1,79 @@
 import React from 'react';
 import Carousel from 'react-native-snap-carousel';
-import DayCard from '../cards/DayCard';
+import WeekCard from '../cards/WeekCard';
 import { Dimensions } from 'react-native';
 
 const screenWidth = Dimensions.get('window').width;
 const itemWidth = screenWidth * 1;
 
+const WeekCarousel = () => {
+  // Tableau de jeux de données de semaines
+  const weekDataArray = [
+    {
+      startDate: 'Lundi 9',
+      endDate: 'Dimanche 15',
+      totalWorkedHours: '24h15',
+      totalOvertimeHours: '4h30',
+      baseSalary: '$2000',
+      numberOfTR: 7,
+      overtimeRate: '20€/h',
+    },
+    {
+      startDate: 'Lundi 16',
+      endDate: 'Dimanche 22',
+      totalWorkedHours: '27h45',
+      totalOvertimeHours: '5h15',
+      baseSalary: '$2100',
+      numberOfTR: 6,
+      overtimeRate: '22€/h',
+    },
+    {
+      startDate: 'Lundi 23',
+      endDate: 'Dimanche 29',
+      totalWorkedHours: '26h30',
+      totalOvertimeHours: '3h45',
+      baseSalary: '$2150',
+      numberOfTR: 8,
+      overtimeRate: '21€/h',
+    },
+    {
+      startDate: 'Lundi 30',
+      endDate: 'Dimanche 5',
+      totalWorkedHours: '28h00',
+      totalOvertimeHours: '6h00',
+      baseSalary: '$2200',
+      numberOfTR: 5,
+      overtimeRate: '23€/h',
+    },
+    {
+      startDate: 'Lundi 6',
+      endDate: 'Dimanche 12',
+      totalWorkedHours: '29h15',
+      totalOvertimeHours: '7h30',
+      baseSalary: '$2250',
+      numberOfTR: 7,
+      overtimeRate: '25€/h',
+    },
+    // Ajoutez d'autres semaines de données ici...
+  ];
 
+    // Inverser l'ordre des données pour afficher la semaine la plus récente en premier
+    const reversedData = [...weekDataArray];
 
-const convertTimeToMinutes = (time) => {
-    const [hours, minutes] = time.split('h').map(Number);
-    return hours * 60 + minutes;
-};
+    // Définir l'index initial sur la dernière semaine
+    const initialIndex = weekDataArray.length - 1;
 
-const calculateDuration = (startTime, endTime) => {
-    return endTime - startTime;
-};
-
-const calculateWorkedHours = (morningCheckIn, morningBreak, afternoonBreak, afternoonCheckIn) => {
-    // Convertir les heures en minutes
-    const morningCheckInMinutes = convertTimeToMinutes(morningCheckIn);
-    const [morningBreakStart, morningBreakEnd] = morningBreak.split(' - ').map(convertTimeToMinutes);
-    const [afternoonBreakStart, afternoonBreakEnd] = afternoonBreak.split(' - ').map(convertTimeToMinutes);
-    const afternoonCheckInMinutes = convertTimeToMinutes(afternoonCheckIn);
-
-    // Calculer la durée travaillée le matin et l'après-midi
-    const morningWorkDuration = calculateDuration(morningCheckInMinutes, morningBreakStart);
-    const morningBreakDuration = calculateDuration(morningBreakStart, morningBreakEnd);
-    const afternoonWorkDuration = calculateDuration(afternoonBreakEnd, afternoonCheckInMinutes);
-    const afternoonBreakDuration = calculateDuration(afternoonBreakStart, afternoonBreakEnd);
-
-    // Calculer le total
-    const totalMinutesWorked = morningWorkDuration + afternoonWorkDuration - morningBreakDuration - afternoonBreakDuration;
-
-    // Convertir le total en heures et minutes
-    const hours = Math.floor(totalMinutesWorked / 60);
-    const minutes = totalMinutesWorked % 60;
-
-    return `${hours}h${minutes}`;
-};
-
-const DayCarousel = () => {
-    // Données fictives avec des pauses
-    const testDaysData = [
-        {
-            date: 'Lundi 9',
-            morningCheckIn: '8h30',
-            morningBreak: '10h30 - 10h45', // Pause du matin
-            afternoonCheckIn: '14h00',
-            afternoonBreak: '16h00 - 16h15', // Pause de l'après-midi
-            workedHours: '5h30',
-        },
-        {
-            date: 'Mardi 10',
-            morningCheckIn: '9h00',
-            morningBreak: '11h00 - 11h15',
-            afternoonCheckIn: '13h00',
-            afternoonBreak: '15h00 - 15h15',
-            workedHours: '4h00',
-        },
-        {
-            date: 'Mercredi 11',
-            morningCheckIn: '9h15',
-            morningBreak: '11h15 - 11h30',
-            afternoonCheckIn: '13h30',
-            afternoonBreak: '15h30 - 15h45',
-            workedHours: '4h15',
-        },
-        // Vous pouvez ajouter d'autres enregistrements ici...
-    ];
-
-       // Calculer les heures travaillées pour chaque jour
-       testDaysData.forEach(day => {
-        day.workedHours = calculateWorkedHours(
-            day.morningCheckIn, day.morningBreak, day.afternoonBreak, day.afternoonCheckIn
-        );
-    });
-
-    const renderItem = ({ item }) => <DayCard {...item} />;
+    const renderItem = ({ item }) => <WeekCard {...item} />;
 
     return (
         <Carousel
-            data={testDaysData}
+            data={reversedData} // Utilisez les données triées dans l'ordre inverse
             renderItem={renderItem}
             sliderWidth={screenWidth}
             itemWidth={itemWidth}
+            firstItem={initialIndex} // Définir l'index initial sur la dernière semaine
         />
     );
 };
 
-export default DayCarousel;
+export default WeekCarousel;
