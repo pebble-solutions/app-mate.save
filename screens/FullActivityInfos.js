048540edcf756d0e4b445b8db8bc291d6440fe27
deathtroll22 <deathtroll@laposte.net>
2024-01-22 12:24:13 +0100

    ajout de la fonction de liasion d'une variable a une activité

--- screens/FullActivityInfos.js
+++ screens/FullActivityInfos.js
@@ -1,9 +1,4 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
-import moment from 'moment';
-import DeleteActivityButton from '../components/deleteActivityButton';
-import { Picker } from '@react-native-picker/picker';
+
 
 //you need to add android:supportsRtl="true" to AndroidManifest.xml
 
@@ -11,76 +6,79 @@ import { Picker } from '@react-native-picker/picker';
 ...
 android:supportsRtl="true"> */}
 
+
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import moment from 'moment';
+import DeleteActivityButton from '../components/deleteActivityButton';
+import { Picker } from '@react-native-picker/picker';
+
 const FullActivityInfos = ({ activity, onClose, onDelete }) => {
   const selectedItem = activity;
   const openSettingsModal = () => {
     console.log('Ouverture des réglages');
   };
   const [variables, setVariables] = useState([]);
-  const [selectedVariable, setSelectedVariable] = useState('');
+  const [selectedVariable, setSelectedVariable] = useState({ label: '', _id: '' });
+  const [activityVariables, setActivityVariables] = useState([]);
+ 
+  const fetchVariables = async () => {
+    try {
+      const response = await fetch('https://api.pebble.solutions/v5/metric/variable/');
+      const data = await response.json();
+      setVariables(data);
+    } catch (error) {
+      console.error('Erreur lors de la récupération des données :', error);
+    }
+  };
+
 
   useEffect(() => {
-    // hook pour effectuer la requête HTTP lors du chargement du composant
-    fetch('https://api.pebble.solutions/v5/metric/variable/')
-      .then(response => response.json())
-      .then(data => {
-        // Une fois les données récupérées, extrayez les labels des variables et mettez-les à jour dans l'état
-        const variableLabels = data.map(variable => variable.label);
-        setVariables(variableLabels);
-      })
-      .catch(error => {
-        console.error('Erreur lors de la récupération des données :', error);
-      });
-  }, []); // Le tableau vide en tant que dépendance signifie que cela ne sera exécuté qu'une seule fois lors du chargement du composant
+    fetchVariables();
+    setActivityVariables(activity.variables);
+  }, [activity]);
 
-  // Fonction pour gérer l'ajout de la variable à l'activité
-const addVariableToActivity = () => {
-  if (selectedVariable) {
-    // Recherchez la variable correspondant au label sélectionné
-    const matchingVariable = variables.find(variable => variable.label === selectedVariable);
-
-    if (matchingVariable) {
-      // Si une variable correspondante est trouvée, utilisez son ID
-      const variableId = matchingVariable._id;
-
-      // Remplacez "activity.id" par la véritable propriété d'ID de votre activité
+ const addVariableToActivity = async () => {
+    if (selectedVariable._id) {
+      const variableId = selectedVariable._id;
       const activityId = activity._id;
-
-      // Effectuez la requête POST vers l'API en utilisant variableId comme ID de variable
       const postData = {
         variable_id: variableId,
         mandatory: false,
         order: 1
       };
 
-      fetch(`https://api.pebble.solutions/v5/activity/${activityId}/metric/variable`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(postData)
-      })
-        .then(response => response.json())
-        .then(data => {
-          // Traitement des données de réponse si nécessaire
-          console.log('Variable ajoutée à l\'activité avec succès :', data);
-        })
-        .catch(error => {
-          console.error('Erreur lors de l\'ajout de la variable à l\'activité :', error);
+      try {
+        const response = await fetch(`https://api.pebble.solutions/v5/activity/${activity._id}/metric/variable`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(postData)
         });
-    } else {
-      console.warn('Aucune variable correspondante trouvée pour le label sélectionné.');
-    }
-  } else {
-    console.warn('Veuillez sélectionner une variable avant de l\'ajouter à l\'activité.');
-  }
-};
 
-  // Fonction pour générer les rectangles verts (4 par ligne) avec "Prénom" et "Nom" sur des lignes distinctes
+        if (response.ok) {
+          console.log('Variable ajoutée à l\'activité avec succès');
+          const newData = await response.json();
+          setActivityVariables(prevVariables => [...prevVariables, newData]);
+          setSelectedVariable({ label: '', _id: '' });
+          fetchVariables();
+        } else {
+          console.error('Erreur lors de l\'ajout de la variable à l\'activité');
+        }
+      } catch (error) {
+        console.error('Erreur lors de l\'ajout de la variable à l\'activité :', error);
+      }
+    } else {
+      console.warn('Veuillez sélectionner une variable avant de l\'ajouter à l\'activité.');
+    }
+  };
+
+
   const renderGreenRectangles = () => {
     const rectangles = [];
-    const numRows = 4; // Nombre de lignes souhaité
-    const numCols = 4; // Nombre de colonnes souhaité
+    const numRows = 4;
+    const numCols = 4;
 
     for (let i = 0; i < numRows; i++) {
       const row = [];
@@ -107,30 +105,21 @@ const addVariableToActivity = () => {
     return rectangles;
   };
 
-
-
-
-
   return (
     <View style={{ ...styles.container, backgroundColor: selectedItem.color }}>
-      {/* Barre supérieure avec bouton de fermeture */}
       <View style={styles.header}>
-        {/* Bouton "Réglages" pour ouvrir la BottomSheet */}
-        <TouchableOpacity onPress={openSettingsModal} style={styles.settingsButton}>
-          <Text style={styles.settingsButtonText}>Réglages</Text>
+        <TouchableOpacity onPress={openSettingsModal} style={styles.closeButton}>
+          <Text style={styles.closeButtonText}>Réglages</Text>
         </TouchableOpacity>
-        {/* Bouton de suppression */}
         <TouchableOpacity onPress={onClose} style={styles.closeButton}>
           <Text style={styles.closeButtonText}>Fermer</Text>
         </TouchableOpacity>
       </View>
 
-      {/* Contenu de l'activité */}
       <View style={styles.activityContent}>
         <Text style={styles.activityName}>{activity.label}</Text>
         <Text style={styles.activityDate}>Créé le {moment(activity.start).format('DD.MM.YYYY')}</Text>
         <Text style={styles.activityDate}>{activity.description}</Text>
-        {/* Ajoutez ici les autres informations de l'activité */}
       </View>
 
       <ScrollView style={styles.scrollView}>
@@ -142,28 +131,28 @@ const addVariableToActivity = () => {
               - {variable.label}
             </Text>
           ))}
-          <Text style={styles.infoSectionTitle}>autres variables disponibles</Text>
+          <Text style={styles.infoSectionTitle}>Autres variables disponibles</Text>
           <Picker
-            selectedValue={selectedVariable}
-            onValueChange={(itemValue) => setSelectedVariable(itemValue)}
+            selectedValue={selectedVariable.label}
+            onValueChange={(itemValue) => {
+              const matchingVariable = variables.find(variable => variable.label === itemValue);
+              setSelectedVariable(matchingVariable || { label: '', _id: '' });
+            }}
           >
             <Picker.Item label="Sélectionnez une variable" value="" />
-            {variables.map((label, index) => (
-              <Picker.Item key={index} label={label} value={label} />
+            {variables.map((variable, index) => (
+              <Picker.Item key={index} label={variable.label} value={variable.label} />
             ))}
           </Picker>
           <TouchableOpacity onPress={addVariableToActivity} style={styles.settingsButton}>
             <Text style={styles.settingsButtonText}>Ajouter cette variable à l'activité</Text>
           </TouchableOpacity>
 
-          <Text> 1   {selectedVariable}</Text>
+          {/* <Text>1   {selectedVariable.label}</Text>
           <Text>2   {selectedVariable._id}</Text>
-          <Text>3   {activity._id}</Text>
-          <Text>4   {variables}</Text>
-
+          <Text>3   {activity._id}</Text> */}
 
         </View>
-        {/* Contenu de la première section */}
         <View style={styles.infoContainer}>
           <Text style={styles.infoSectionTitle}>Collaborateurs</Text>
           {/* Affichage des rectangles verts (4 par ligne) */}
@@ -176,18 +165,17 @@ const addVariableToActivity = () => {
           <Text style={styles.infoSectionContent}>- autre 2</Text>
           <Text style={styles.infoSectionContent}>- autre 3</Text>
         </View>
-        {/* Contenu de la troisième section */}
         <DeleteActivityButton
           title={selectedItem.label}
           id={selectedItem._id}
         />
       </ScrollView>
 
-      {/* Cette View garantit que le ScrollView termine à 10% du bas de l'écran */}
       <View />
     </View>
   );
 };
+
 
 const styles = StyleSheet.create({
   container: {
@@ -200,10 +188,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingVertical: 25,
   },
-  settingsButtonText: {
-    color: 'white',
-    fontSize: 14,
+  settingsButton: {
+    backgroundColor: '#007bff', // Bleu classique pour les boutons
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
+  settingsButtonText: {
+    color: 'white', // Texte blanc pour une meilleure visibilité
+    fontWeight: 'bold',
+  },
+
   closeButtonText: {
     color: 'white',
     fontSize: 14,
